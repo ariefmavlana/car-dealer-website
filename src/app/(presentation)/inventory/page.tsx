636396428f8fc50b18c7bdbb7ad2aa2bd56e8b08
ariefmavlana <ariefmaulana0808@gmail.com>
2025-06-30 21:19:43 +0700

    izin serius bang, comm one

---
+++ src/app/(presentation)/inventory/page.tsx
@@ -0,0 +1,184 @@
+import { ClassifiedsList } from "@/components/inventory/classifieds-list";
+import { DialogFilters } from "@/components/inventory/dialog-filters";
+import { Sidebar } from "@/components/inventory/sidebar";
+import { CustomPagination } from "@/components/shared/custom-pagination";
+import { CLASSIFIEDS_PER_PAGE } from "@/config/constants";
+import { routes } from "@/config/routes";
+import type { AwaitedPageProps, Favourites, PageProps } from "@/config/types";
+import { prisma } from "@/lib/prisma";
+import { redis } from "@/lib/redis-store";
+import { getSourceId } from "@/lib/source-id";
+import { ClassifiedStatus, Prisma } from "@prisma/client";
+import { z } from "zod";
+
+const PageSchema = z.string().transform((val) => Math.max(Number(val), 1)).optional()
+
+const ClassifiedFilterSchema = z.object({
+  q: z.string().optional(),
+    make: z.string().optional(),
+    model: z.string().optional(),
+    modelVariant: z.string().optional(),
+    minYear: z.string().optional(),
+    maxYear: z.string().optional(),
+    minPrice: z.string().optional(),
+    maxPrice: z.string().optional(),
+    minReading: z.string().optional(),
+    maxReading: z.string().optional(),
+    currency: z.string().optional(),
+    odoUnit: z.string().optional(),
+    transmission: z.string().optional(),
+    fuelType: z.string().optional(),
+    bodyType: z.string().optional(),
+    colour: z.string().optional(),
+    doors: z.string().optional(),
+    seats: z.string().optional(),
+    ulezCompliance: z.string().optional(),
+})
+
+const buildClassifiedFilterQuery = (searchParams: AwaitedPageProps['searchParams'] | undefined): Prisma.ClassifiedWhereInput => {
+  const { data } = ClassifiedFilterSchema.safeParse(searchParams)
+
+  if (!data) return { status: ClassifiedStatus.LIVE }
+
+  const keys = Object.keys(data)
+
+  const taxonomyFilters = ["make", "model", "modelVariant"]
+
+  const rangeFilters = {
+    minYear: "year",
+    maxYear: "year",
+    minPrice: "price",
+    maxPrice: "price",
+    minReading: "odoReading",
+    maxReading: "odoReading",
+  }
+
+  const numFilters = ["seats", "doors"]
+  const enumFilters = ["odoUnit", "currency", "transmission", "bodyType", "fuelType", "colour", "ulezCompliance"]
+
+  const mapParamsToFields = keys.reduce((acc, key) => {
+    const value = searchParams?.[key] as string | undefined
+    if (!value) return acc
+
+    if (taxonomyFilters.includes(key)) {
+      acc[key] = { id: Number(value) }
+    } else if (enumFilters.includes(key)) {
+      acc[key] = value.toUpperCase()
+    } else if (numFilters.includes(key)) {
+      acc[key] = Number(value)
+    }
+     else if (key in rangeFilters) {
+      const field = rangeFilters[key as keyof typeof rangeFilters]
+      acc[field] = acc[field] || {}
+      if (key.startsWith("min")) {
+        acc[field].gte = Number(value)
+      } else if (key.startsWith("max")) {
+        acc[field].lte = Number(value)
+      }
+    }
+
+    return acc
+  }, {} as {[key: string]: any},)
+  return {
+    status: ClassifiedStatus.LIVE,
+
+    ...(searchParams?.q && {
+      OR: [
+        {
+          title: {
+            contains: searchParams.q as string,
+            mode: "insensitive"
+          }
+        },
+        {
+          description: {
+            contains: searchParams.q as string,
+            mode: "insensitive"
+          }
+        }
+      ]
+    }),
+
+    ...mapParamsToFields
+  }
+}
+
+const getInventory = async (searchParams: AwaitedPageProps['searchParams']) => {
+  const validPage = PageSchema.parse(searchParams?.page)
+
+  // Get the current page
+  const page = validPage ? validPage : 1
+
+  // Calculate the offset
+  const offset = (page - 1) * CLASSIFIEDS_PER_PAGE
+
+  return prisma.classified.findMany({
+    where: buildClassifiedFilterQuery(searchParams),
+    include: { images: { take: 1 } },
+    skip: offset,
+    take: CLASSIFIEDS_PER_PAGE
+  })
+}
+
+export default async function InventoryPage(props: PageProps) {
+  const searchParams = await props.searchParams
+  const classifieds = await getInventory(searchParams)
+  const count = await prisma.classified.count({
+    where: buildClassifiedFilterQuery(searchParams),
+  })
+
+  const minMaxResult = await prisma.classified.aggregate({
+    where: { status: ClassifiedStatus.LIVE },
+    _min: {
+      year: true,
+    },
+    _max: {
+      price: true,
+      year: true,
+      odoReading: true,
+    }
+  })
+
+  const sourceId = await getSourceId()
+  const favourites = await redis.get<Favourites>(sourceId ?? "")
+  const totalPages = Math.ceil(count / CLASSIFIEDS_PER_PAGE)
+
+  return (
+    <div className="flex">
+      <Sidebar minMaxValues={minMaxResult} searchParams={searchParams}/>
+
+      <div className="flex-1 p-4 bg-white">
+        <div className="flex space-y-2 flex-col items-center justify-center pb-4 -mt-1">
+          <div className="flex justify-between items-center w-full">
+            <h2 className="text-gray-700 text-sm md:text-base lg:text-xl font-semibold min-w-fit">
+              We have found {count} classifieds...
+            </h2>
+            <DialogFilters minMaxValues={minMaxResult} count={count} searchParams={searchParams}/>
+          </div>
+          <CustomPagination
+            baseURL={routes.inventory}
+            totalPages={totalPages}
+            styles={{
+              paginationRoot: "flex justify-end hidden lg:block",
+              paginationPrevious: "",
+              paginationNext: "",
+              paginationLink: "border-none active:border text-black",
+              paginationLinkActive: "",
+            }}/>
+        </div>
+
+        <ClassifiedsList classifieds={classifieds} favourites={favourites ? favourites.ids : []}/>
+        <CustomPagination
+            baseURL={routes.inventory}
+            totalPages={totalPages}
+            styles={{
+              paginationRoot: "justify-center lg:hidden pt-12",
+              paginationPrevious: "",
+              paginationNext: "",
+              paginationLink: "border-none active:border",
+              paginationLinkActive: "",
+            }}/>
+      </div>
+    </div>
+  )
+}
---
+++ src/lib/utils.ts
@@ -0,0 +1,118 @@
+import { BodyType, Colour, CurrencyCode, FuelType, OdoUnit, Transmission, ULEZCompliance } from "@prisma/client";
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+/**
+ * Combines multiple class names and merges Tailwind CSS classes efficiently
+ * @param inputs - Class names to combine and merge
+ * @returns Merged class names string
+ */
+export function cn(...inputs: ClassValue[]) {
+	return twMerge(clsx(inputs));
+}
+
+interface FormatPriceArgs {
+	price: number | null;
+	currency: CurrencyCode | null;
+}
+
+export function formatUlezCompliance(ulezCompliance: ULEZCompliance) {
+	return ulezCompliance === ULEZCompliance.EXEMPT ? "Exempt" : "Non-Exempt";
+}
+
+export function formatBodyType(bodyType: BodyType) {
+	switch (bodyType) {
+		case BodyType.CONVERTIBLE:
+			return "Convertible";
+		case BodyType.COUPE:
+			return "Coupe";
+		case BodyType.HATCHBACK:
+			return "Hatchback";
+		case BodyType.SUV:
+			return "SUV";
+		case BodyType.WAGON:
+			return "Wagon";
+		case BodyType.SEDAN:
+			return "Sedan";
+		default:
+			return "Unknown";
+	}
+}
+
+export function formatPrice({ price, currency }: FormatPriceArgs) {
+	if (!price) return "0";
+
+	const formatter = new Intl.NumberFormat("en-GB", {
+		style: "currency",
+		currencyDisplay: "narrowSymbol",
+		...(currency && { currency }),
+		maximumFractionDigits: 0,
+	});
+
+	return formatter.format(price / 100);
+}
+
+export function formatNumber(
+	num: number | null,
+	options?: Intl.NumberFormatOptions,
+) {
+	if (!num) return "0";
+
+	return new Intl.NumberFormat("en-GB", options).format(num);
+}
+
+export function formatOdometerUnit(unit: OdoUnit) {
+	return unit === OdoUnit.MILES ? "mi" : "km";
+}
+
+export function formatTransmission(transmission: Transmission) {
+	return transmission === Transmission.AUTOMATIC ? "Automatic" : "Manual";
+}
+
+export function formatFuelType(fuelType: FuelType) {
+	switch (fuelType) {
+		case FuelType.PETROL:
+			return "Petrol";
+		case FuelType.DIESEL:
+			return "Diesel";
+		case FuelType.ELECTRIC:
+			return "Electric";
+		case FuelType.HYBRID:
+			return "Hybrid";
+		default:
+			return "Unknown";
+	}
+}
+
+export function formatColour(colour: Colour) {
+	switch (colour) {
+		case Colour.BLACK:
+			return "Black";
+		case Colour.WHITE:
+			return "White";
+		case Colour.SILVER:
+			return "Silver";
+		case Colour.RED:
+			return "Red";
+		case Colour.BLUE:
+			return "Blue";
+		case Colour.BROWN:
+			return "Brown";
+		case Colour.GOLD:
+			return "Gold";
+		case Colour.GREEN:
+			return "Green";
+		case Colour.GREY:
+			return "Grey";
+		case Colour.ORANGE:
+			return "Orange";
+		case Colour.PINK:
+			return "Pink";
+		case Colour.PURPLE:
+			return "Purple";
+		case Colour.YELLOW:
+			return "Yellow";
+		default:
+			return "Unknown";
+	}
+}
